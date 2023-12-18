56cf36713a960b762aa4c4dc2d42329299b7dae1
Alexander Henting <a.henting@posteo.de>
2023-12-18 12:09:04 +0100

    feat: email notifications

---
+++ src/components/VerifyEmail.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { trpc } from '@/trpc/client';
+import { Loader2, XCircle } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
+
+interface VerifyEmailProps {
+  token: string;
+}
+function VerifyEmail({ token }: VerifyEmailProps) {
+  const { data, isLoading, isError } = trpc.auth.verifyEmail.useQuery({
+    token,
+  });
+
+  if (isError) {
+    return (
+      <div className={'flex flex-col items-center gap-2'}>
+        <XCircle className={'h-8 w-8 text-red-800'} />
+        <h3 className={'text-xl font-semibold'}>There was a problem.</h3>
+        <p className={'text-sm text-muted-foreground'}>
+          This token is not valid or might be expired. Please try again.
+        </p>
+      </div>
+    );
+  }
+
+  if (data?.success) {
+    return (
+      <div className={'flex h-full flex-col items-center justify-center'}>
+        <div className={'relative mb-4 h-60 w-60 text-muted-foreground'}>
+          <Image src={'/orange-send-email.png'} alt={'email send'} fill />
+        </div>
+        <h3 className={'text-2xl font-semibold'}>You&apos;re all set!</h3>
+        <p className={'mt-1 text-center text-muted-foreground'}>
+          Thank you for verifying your email.
+        </p>
+        <Link
+          className={buttonVariants({ className: 'mt-4' })}
+          href={'/sign-in'}
+        >
+          Sign in
+        </Link>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className={'flex flex-col items-center gap-2'}>
+        <Loader2 className={'h-8 w-8 animate-spin text-zinc-300'} />
+        <h3 className={'text-xl font-semibold'}>Verifying....</h3>
+        <p className={'text-sm text-muted-foreground'}>
+          This won&apos;t take long.
+        </p>
+      </div>
+    );
+  }
+
+  return <div></div>;
+}
+
+export default VerifyEmail;
